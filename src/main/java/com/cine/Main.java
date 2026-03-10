d62b14aebcf1c0b27a5bf1a492c33119cad14bb8
Esteban77orjuela <esteban77orjuela@gmail.com>
2026-03-10 17:19:37 -0500

    feat: inicio del proyecto E-Commerce

---
+++ src/main/java/com/cine/Main.java
@@ -0,0 +1,18 @@
+package com.cine;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String proyecto = "E-Commerce";
+        String dev = "TU NOMBRE";
+        int anio = 2025;
+        boolean listo = true;
+
+        System.out.println("=================================");
+        System.out.println("Proyecto: " + proyecto);
+        System.out.println("Dev: " + dev);
+        System.out.println("Anio: " + anio);
+        System.out.println("Listo: " + listo);
+        System.out.println("=================================");
+    }
+}
